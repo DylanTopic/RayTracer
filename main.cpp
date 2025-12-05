f27b1f8b371bd35e8a984dd993eb6bd6a42e484b
DylanTopic <93812995+DylanTopic@users.noreply.github.com>
2025-12-04 23:03:27 -0600

    Chapter 3
    Complete implementation of chapter 3 from the book

--- main.cpp
+++ main.cpp
@@ -1,34 +1,26 @@
+#include "color.h"
+#include "vec3.h"
+
 #include <iostream>
-#include <fstream> 
+#include <fstream>
 
 int main() {
-    int image_width = 256;
-    int image_height = 256;
 
-    std::ofstream outfile("image.ppm");
+    const int image_width = 256;
+    const int image_height = 256;
+
+    std::ofstream outfile("image.ppm"); 
 
     outfile << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-    for (int j = 0; j < image_height; ++j) {
-
-        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-
+    for (int j = image_height - 1; j >= 0; --j) {
+        std::clog << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0.0;
-
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
-
-            outfile << ir << ' ' << ig << ' ' << ib << '\n';
+            color pixel_color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.25);
+            write_color(outfile, pixel_color);
         }
     }
 
-    std::clog << "\rDone.                 \n";
-
+    std::clog << "\nDone.\n";
     outfile.close();
-    return 0;
 }
