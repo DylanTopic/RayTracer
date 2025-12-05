c73e343811897c3ee31e7cf3bda97880043fcbe8
DylanTopic <93812995+DylanTopic@users.noreply.github.com>
2025-12-04 22:49:39 -0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream> 
+
+int main() {
+    int image_width = 256;
+    int image_height = 256;
+
+    std::ofstream outfile("image.ppm");
+
+    outfile << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = 0; j < image_height; ++j) {
+
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+
+        for (int i = 0; i < image_width; ++i) {
+
+            auto r = double(i) / (image_width - 1);
+            auto g = double(j) / (image_height - 1);
+            auto b = 0.0;
+
+            int ir = int(255.999 * r);
+            int ig = int(255.999 * g);
+            int ib = int(255.999 * b);
+
+            outfile << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+
+    std::clog << "\rDone.                 \n";
+
+    outfile.close();
+    return 0;
+}
