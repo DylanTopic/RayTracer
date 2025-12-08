ca57ecd460ff418761a72191cae9956c55c44a19
DylanTopic <93812995+DylanTopic@users.noreply.github.com>
2025-12-08 02:24:38 -0600

    Chapter 8
    Full implementation of chapter 8, antialiasing fully adding (blurry circle 😼)

--- camera.h
+++ camera.h
@@ -9,6 +9,7 @@ class camera {
 public:
     double aspect_ratio = 1.0;  
     int    image_width = 100;  
+    int    samples_per_pixel = 10;
     void render(const hittable& world) {
         initialize();
 
@@ -25,7 +26,13 @@ public:
                 ray r(center, ray_direction);
                 color pixel_color = ray_color(r, world);
 
-                write_color(outfile, pixel_color);
+                for (int sample = 0; sample < samples_per_pixel; ++sample) {
+                    ray r = get_ray(i, j);
+                    pixel_color += ray_color(r, world);
+                }
+
+                
+                write_color(outfile, pixel_samples_scale * pixel_color);
             }
         }
 
@@ -35,6 +42,7 @@ public:
 
 private:
     int    image_height;   
+    double pixel_samples_scale;
     point3 center;         
     point3 pixel00_loc;    
     vec3   pixel_delta_u;  
@@ -43,6 +51,8 @@ private:
     void initialize() {
         image_height = int(image_width / aspect_ratio);
         image_height = (image_height < 1) ? 1 : image_height;
+
+        pixel_samples_scale = 1.0 / samples_per_pixel;
 
         center = point3(0, 0, 0);
 
@@ -58,6 +68,22 @@ private:
 
         auto viewport_upper_left = center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
         pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+    }
+
+    ray get_ray(int i, int j) const {
+        auto offset = sample_square();
+        auto pixel_sample = pixel00_loc
+            + ((i + offset.x()) * pixel_delta_u)
+            + ((j + offset.y()) * pixel_delta_v);
+
+        auto ray_origin = center;
+        auto ray_direction = pixel_sample - ray_origin;
+
+        return ray(ray_origin, ray_direction);
+    }
+
+    vec3 sample_square() const {
+        return vec3(random_double() - 0.5, random_double() - 0.5, 0);
     }
 
     color ray_color(const ray& r, const hittable& world) const {
--- rtweekend.h
+++ rtweekend.h
@@ -6,7 +6,7 @@
 #include <limits>
 #include <memory>
 #include <vector>
-
+#include <cstdlib>
 
 using std::make_shared;
 using std::shared_ptr;
@@ -18,6 +18,15 @@ const double pi = 3.1415926535897932385;
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
 }
+
+inline double random_double() {
+    return std::rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    return min + (max - min) * random_double();
+}
+
 
 #include "interval.h" 
 #include "ray.h"
