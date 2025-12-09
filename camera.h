205e8a4ef6d5dac16be763baf35d4a9aab4d8456
DylanTopic <93812995+DylanTopic@users.noreply.github.com>
2025-12-09 08:56:35 -0600

    Chapter 12 1/2
    First half of chapter 12

--- camera.h
+++ camera.h
@@ -12,6 +12,7 @@ public:
     int    image_width = 100;  
     int    samples_per_pixel = 10;
     int    max_depth = 10;
+    double vfov = 90;
     void render(const hittable& world) {
         initialize();
 
@@ -56,7 +57,9 @@ private:
         center = point3(0, 0, 0);
 
         auto focal_length = 1.0;
-        auto viewport_height = 2.0;
+        auto theta = degrees_to_radians(vfov);
+        auto h = std::tan(theta / 2);
+        auto viewport_height = 2 * h * focal_length;
         auto viewport_width = viewport_height * (double(image_width) / image_height);
 
         auto viewport_u = vec3(viewport_width, 0, 0);
