3bd23c1e122c6400eb8fb6600959af8ff302693e
DylanTopic <93812995+DylanTopic@users.noreply.github.com>
2025-12-13 23:03:23 -0600

    Chapter 4 1/2
    Half implementation of chapter 4

--- hittable.h
+++ hittable.h
@@ -13,6 +13,8 @@ public:
     shared_ptr<material> mat;
     double t;
     bool front_face;
+    double u;
+    double v;
 
     void set_face_normal(const ray& r, const vec3& outward_normal) {
         front_face = dot(r.direction(), outward_normal) < 0;
--- main.cpp
+++ main.cpp
@@ -5,39 +5,41 @@
 #include "hittable_list.h"
 #include "material.h"
 #include "sphere.h"
+#include "texture.h"
 
 int main() {
     hittable_list world;
-
-    auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
+    auto checker = make_shared<checker_texture>(0.32, color(.2, .3, .1), color(.9, .9, .9));
+    world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, make_shared<lambertian>(checker)));
 
     auto material_center = make_shared<lambertian>(color(0.4, 0.2, 0.1));
     auto center1 = point3(0.0, 0.0, -1.0);
-    auto center2 = point3(0.0, 0.5, -1.0); 
+    auto center2 = point3(0.0, 0.5, -1.0);
     world.add(make_shared<sphere>(center1, center2, 0.5, material_center));
+
     auto material_left = make_shared<dielectric>(1.5);
     world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
     world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.4, make_shared<dielectric>(1.00 / 1.50)));
 
     auto material_right = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
     world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
+
     world = hittable_list(make_shared<bvh_node>(world));
 
     camera cam;
 
     cam.aspect_ratio = 16.0 / 9.0;
-    cam.image_width = 400; 
+    cam.image_width = 400;
     cam.samples_per_pixel = 100;
     cam.max_depth = 50;
 
     cam.vfov = 20;
-    cam.lookfrom = point3(13, 2, 3);
-    cam.lookat = point3(0, 0, 0);
+    cam.lookfrom = point3(0, 2, 6);
+    cam.lookat = point3(0, 0, -1);
     cam.vup = vec3(0, 1, 0);
 
     cam.defocus_angle = 0.6;
-    cam.focus_dist = 10.0;
+    cam.focus_dist = 7.28;
 
     cam.render(world);
 }
