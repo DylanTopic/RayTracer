8db9afe92af9e50a1137d78a2b6f796d3223c01f
DylanTopic <93812995+DylanTopic@users.noreply.github.com>
2025-12-14 02:41:38 -0600

    Book 2 Chapter 6
    Complete implementation of Quadrilaterals from book 2 chapter 6

--- main.cpp
+++ main.cpp
@@ -5,41 +5,34 @@
 #include "hittable_list.h"
 #include "material.h"
 #include "sphere.h"
-#include "texture.h" 
+#include "texture.h"
+#include "quad.h" 
 
-void perlin_ground() {
+void quads() {
     hittable_list world;
 
-    // --- CHANGED: Ground now uses Noise Texture ---
-    // Scale = 4.0 creates a standard marble pattern size on the ground
-    auto pertext_ground = make_shared<noise_texture>(4.0);
-    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, make_shared<lambertian>(pertext_ground)));
-    // ----------------------------------------------
+    auto left_red = make_shared<lambertian>(color(1.0, 0.2, 0.2));
+    auto back_green = make_shared<lambertian>(color(0.2, 1.0, 0.2));
+    auto right_blue = make_shared<lambertian>(color(0.2, 0.2, 1.0));
+    auto upper_orange = make_shared<lambertian>(color(1.0, 0.5, 0.0));
+    auto lower_teal = make_shared<lambertian>(color(0.2, 0.8, 0.8));
 
-    // 2. Left Sphere: Low Frequency Noise
-    auto tex_low = make_shared<noise_texture>(0.5);
-    world.add(make_shared<sphere>(point3(-2.2, 1, 0), 1.0, make_shared<lambertian>(tex_low)));
-
-    // 3. Center Sphere: Medium Frequency Noise
-    auto tex_med = make_shared<noise_texture>(4.0);
-    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, make_shared<lambertian>(tex_med)));
-
-    // 4. Right Sphere: High Frequency Noise
-    auto tex_high = make_shared<noise_texture>(10.0);
-    world.add(make_shared<sphere>(point3(2.2, 1, 0), 1.0, make_shared<lambertian>(tex_high)));
+    world.add(make_shared<quad>(point3(-3, -2, 5), vec3(0, 0, -4), vec3(0, 4, 0), left_red));
+    world.add(make_shared<quad>(point3(-2, -2, 0), vec3(4, 0, 0), vec3(0, 4, 0), back_green));
+    world.add(make_shared<quad>(point3(3, -2, 1), vec3(0, 0, 4), vec3(0, 4, 0), right_blue));
+    world.add(make_shared<quad>(point3(-2, 3, 1), vec3(4, 0, 0), vec3(0, 0, 4), upper_orange));
+    world.add(make_shared<quad>(point3(-2, -3, 5), vec3(4, 0, 0), vec3(0, 0, -4), lower_teal));
 
     camera cam;
 
-    cam.aspect_ratio = 16.0 / 9.0;
+    cam.aspect_ratio = 1.0;
     cam.image_width = 400;
     cam.samples_per_pixel = 100;
     cam.max_depth = 50;
 
-    cam.vfov = 20;
-
-    // Look from slightly higher up to see the ground better
-    cam.lookfrom = point3(0, 4, 10);
-    cam.lookat = point3(0, 1, 0);
+    cam.vfov = 80;
+    cam.lookfrom = point3(0, 0, 9);
+    cam.lookat = point3(0, 0, 0);
     cam.vup = vec3(0, 1, 0);
 
     cam.defocus_angle = 0;
@@ -48,5 +41,5 @@ void perlin_ground() {
 }
 
 int main() {
-    perlin_ground();
+    quads();
 }
